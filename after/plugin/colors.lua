637e3d1c3df44a130730e61fadb66332529d2738
984345 <rohanmainali39@gmail.com>
2023-01-14 15:55:06 +0545

    vs code color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,16 +1,5 @@
-function ColorMyPencil(color)
-	color = color or "rose-pine"
-	vim.cmd([[
-	try
-	colorscheme rose-pine
-	catch /^Vim\%((\a\+)\)\=:E185/
-	colorscheme default 
-	set background=dark
-	endtry
-	]])
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+function ColorMyPencil()
+    require('vscode').change_style('dark')
 end
 
 ColorMyPencil()

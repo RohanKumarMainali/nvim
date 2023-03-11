da4c1c18a2267bec4938221cdd72ea5d448e7203
RohanKumarMainali <rohanmainali39@gmail.com>
2023-03-11 21:52:34 +0545

    ignore node_modules

--- nvim/lua/therohan/packer.lua
+++ nvim/lua/therohan/packer.lua
@@ -10,6 +10,9 @@ return require("packer").startup(function(use)
     use({
         "nvim-telescope/telescope.nvim",
         tag = "0.1.0",
+        defaults = {
+        file_ignore_patterns = {"^node_modules/"},
+    },
         -- or                            , branch = '0.1.x',
         requires = { { "nvim-lua/plenary.nvim" } },
     })
